1e060798400eca00288bdb3072221a166a064c04
berto <berto.ort@gmail.com>
2020-04-06 13:40:12 -1000

    initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SuitEnum
+{
+    Hearts = 1,
+    Clubs = 2,
+    Diamonds = 3,
+    Spades = 4,
+}
+
+public enum RankEnum
+{
+    Ace = 1,
+    King = 2,
+    Queen = 3,
+    Jack = 4,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+}
+
+public class Card : MonoBehaviour
+{
+    [SerializeField]
+    private SuitEnum _suit;
+    [SerializeField]
+    private RankEnum _rank;
+    [SerializeField]
+    private string _id;
+    
+    public SuitEnum Suit { get { return _suit; } set { _suit = value; } }
+    public RankEnum Rank { get { return _rank; } set { _rank = value; } }
+
+
+    public void Init(SuitEnum suit, RankEnum rank)
+    {
+        _id = string.Format("Card_{0}_{1}", suit, rank);
+        gameObject.name = _id;
+        _suit = suit;
+        _rank = rank;
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _cardPrefab;
+
+    private List<Card> _deck = new List<Card>();
+    private List<Card> _discardPile = new List<Card>();
+
+    public Card TakeCard()
+    {
+        if (_deck.Count == 0)
+            return null;
+
+        Card Card = _deck[0];
+        _deck.RemoveAt(0);
+
+        return Card;
+    }
+
+    public void Awake()
+    {
+        foreach (SuitEnum suit in System.Enum.GetValues(typeof(SuitEnum)))
+        {
+            foreach (RankEnum rank in System.Enum.GetValues(typeof(RankEnum)))
+            {
+                GameObject cardInstance = Instantiate(_cardPrefab);
+                Card card = cardInstance.GetComponent<Card>();
+                if (card != null)
+                {
+                    card.Init(suit, rank);
+                    _deck.Add(card);
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Card card = TakeCard();
+            Debug.Log(card);
+        }
+    }
+
+}
